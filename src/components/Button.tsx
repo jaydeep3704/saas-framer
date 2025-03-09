6cbc8052f9e535d189a58a9db481a659a9ac13c0
Jaydeep <jaydeeppatil3704@gmail.com>
2025-03-10 00:59:21 +0530

    Sections Done : Navbar, Hero section

---
+++ src/components/Button.tsx
@@ -0,0 +1,32 @@
+import React, { ButtonHTMLAttributes, HTMLAttributes } from 'react'
+import { cva } from 'class-variance-authority';
+
+const classes=cva('px-8 font-medium border  h-12 rounded-full',{
+    variants:{
+        variant:{
+            primary:'bg-lime-400 text-neutral-950 border-lime-400',
+            secondary:'border-white text-white bg-transparent'
+        },
+        size:{
+            sm:"h-10"
+        }
+
+    }
+
+})
+
+
+
+const Button = (props:{variant:"primary"|"secondary",size?:"sm"} & ButtonHTMLAttributes<HTMLButtonElement>) => {
+    
+
+  
+    const {variant,className,size,...otherProps}=props
+    
+    return (
+    <button className={classes({variant,size,className})} {...otherProps}/>
+
+  )
+}
+
+export default Button
---
+++ src/components/Pointer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Pointer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Pointer
